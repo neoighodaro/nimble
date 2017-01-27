1b053f404e91dfd2844dfef8c741579e79cd2b06
Mozartted <mozart.osita@gmail.com>
2017-01-28 00:02:31 +0100

    Added a tag line to the welcome page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,9 @@ class HomeController extends Controller {
     public function index()
     {
         return view('welcome', [
-            'sitename' => config('app.name')
+            'sitename' => config('app.name'),
+            'tag'      => config('app.tag')
         ]);
     }
+    
 }
